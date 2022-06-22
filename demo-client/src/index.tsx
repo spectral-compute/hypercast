632b0bc48aeadd03575dccc1645b3aae629fb8f7
Nicholas Tomlinson <nstomlinson@gmail.com>
2022-06-22 03:01:57 +0100

    [Demo-Client] Make the demo client actually be a video streamer client

--- demo-client/src/index.tsx
+++ demo-client/src/index.tsx
@@ -2,6 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 
+import * as lvsc from "live-video-streamer-client";
+
+declare namespace process {
+    let env: {
+        REACT_APP_INFO_URL: string;
+    };
+}
+
+const player: lvsc.Player = new lvsc.Player(process.env.REACT_APP_INFO_URL,
+                                            document.getElementById("video")! as HTMLVideoElement, null);
+
 function App(): React.ReactElement<HTMLDivElement> {
     return (<div></div>);
 }
@@ -12,3 +23,10 @@ root.render(
         <App />
     </React.StrictMode>
 );
+
+/* Start. */
+async function init(): Promise<void> {
+    await player.init();
+    player.start();
+}
+void init();
