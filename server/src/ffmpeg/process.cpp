620662a820d8e4ff0fd096324c1f1df94f23dfbc
Nicholas Tomlinson <nstomlinson@gmail.com>
2023-03-24 04:05:01 +0000

    Make Ffmpeg::FfmpegProcess log its arguments

--- server/src/ffmpeg/process.cpp
+++ server/src/ffmpeg/process.cpp
@@ -9,6 +9,40 @@ namespace
 {
 
 /**
+ * Escape a character so it's unambiguous when displayed in a string.
+ */
+const char *escapeChar(char c)
+{
+    switch (c) {
+        case '\\': return "\\\\";
+        case '"': return "\\\"";
+    }
+    return nullptr;
+}
+
+/**
+ * Format an array of arguments for display as a single string.
+ */
+std::string getArgumentsForLog(std::span<const std::string> arguments)
+{
+    std::string result;
+    for (const std::string &argument: arguments) {
+        result += '"';
+        for (char c: argument) {
+            if (const char *s = escapeChar(c)) {
+                result += s;
+            }
+            else {
+                result += c;
+            }
+        }
+        result += "\", ";
+    }
+    result.resize(result.size() - 2); // Remove the trailing ", ".
+    return result;
+}
+
+/**
  * Handle a line from ffmpeg's stderr.
  */
 void handleFfmpegStderrLine(Log::Context &log, std::string_view line)
@@ -31,6 +65,9 @@ void handleFfmpegStderrLine(Log::Context &log, std::string_view line)
 Ffmpeg::FfmpegProcess::FfmpegProcess(IOContext &ioc, Log::Log &log, std::span<const std::string> arguments) :
     log(log("ffmpeg")), subprocess(ioc, "ffmpeg", arguments, false, false)
 {
+    /* Log the arguments given to ffmpeg. */
+    this->log << "arguments" << Log::Level::info << getArgumentsForLog(arguments);
+
     /* Create a coroutine to handle the stderr output of ffmpeg and wait for the process termination. */
     spawnDetached(ioc, this->log, [this]() -> Awaitable<void> {
         while (auto line = co_await this->subprocess.readStderrLine()) {
