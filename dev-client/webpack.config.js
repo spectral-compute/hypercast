4a9e9c5eeefb27ca450f2d70c12fffee42285f3e
Chris Kitching <chriskitching@linux.com>
2023-03-31 05:26:35 +0100

    Fix some JS buildysstem nonsense

--- dev-client/webpack.config.js
+++ dev-client/webpack.config.js
@@ -2,43 +2,6 @@ const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 const path = require("path");
 
-var config = {
-    entry: "./app.ts",
-    output: {
-        filename: "app.js",
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: [
-                    "babel-loader",
-                    {
-                        loader: "ts-loader",
-                        options: {
-                            projectReferences: true
-                        }
-                    }
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                "./index.html"
-            ]
-        })
-    ],
-    resolve: {
-        alias: {
-            'live-video-streamer-client': path.resolve(__dirname, '../client/src'),
-        },
-        extensions: [".ts", ".js"]
-    }
-};
-
 module.exports = (env, argv) => {
     if (argv.mode === "development") {
         config.devtool = "source-map";
@@ -47,10 +10,48 @@ module.exports = (env, argv) => {
     const defEnv = (name, def) => {
         return JSON.stringify((env[name] === undefined) ? def : env[name]);
     };
-    config.plugins.push(new webpack.DefinePlugin({
-        "process.env.INFO_URL": defEnv("INFO_URL", null),
-        "process.env.SECONDARY_VIDEO": defEnv("SECONDARY_VIDEO", false)
-    }));
 
-    return config;
+    return {
+        entry: "./app.ts",
+        mode: (env["NODE_ENV"] === "development" ? "development" : "production"),
+        devtool: 'source-map',
+        output: {
+            filename: "app.js",
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        "babel-loader",
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                projectReferences: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    "./index.html"
+                ]
+            }),
+            new webpack.DefinePlugin({
+                "process.env.INFO_URL": defEnv("INFO_URL", null),
+                "process.env.SECONDARY_VIDEO": defEnv("SECONDARY_VIDEO", false),
+                "process.env.NODE_DEBUG": defEnv("NODE_DEBUG", undefined)
+            })
+        ],
+        resolve: {
+            alias: {
+                'live-video-streamer-client': path.resolve(__dirname, '../client/src'),
+            },
+            extensions: [".ts", ".js"]
+        }
+    };
 };
