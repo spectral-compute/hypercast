56b863b778cb71441be086ff40c2bd71706d24ac
Nicholas Tomlinson <nstomlinson@gmail.com>
2023-04-01 12:36:55 +0100

    Correctly identify the MIME types of various web text files

--- server/src/resources/FilesystemResource.cpp
+++ server/src/resources/FilesystemResource.cpp
@@ -6,6 +6,45 @@
 #include "util/File.hpp"
 #include "util/subprocess.hpp"
 
+namespace
+{
+
+/**
+ * The set of built-in known MIME types.
+ *
+ * This is useful because some important text-based formats are not always correctly identified by file.
+ */
+constexpr std::pair<std::string_view, std::string_view> mimeTypes[] = {
+    { "css", "text/css" },
+    { "html", "text/html" },
+    { "js", "text/javascript" },
+    { "json", "application/json" },
+    { "svg", "image/svg+xml" }
+};
+
+/**
+ * Get the MIME type of a file.
+ */
+Awaitable<std::string> getMimeTypeForFile(IOContext &ioc, const std::filesystem::path &path)
+{
+    /* See if we know about the MIME type by the filename's extension. */
+    std::string extension = path.extension().string().substr(1);
+    for (auto [candidateExtension, candidateMimeType]: mimeTypes) {
+        if (candidateExtension == extension) {
+            co_return std::string(candidateMimeType);
+        }
+    }
+
+    /* Otherwise, use file to identify the MIME type. */
+    std::string mimeType = co_await Subprocess::getStdout(ioc, "file", {"-bEL", "--mime-type", path.string()});
+    if (!mimeType.empty() && mimeType.back() == '\n') {
+        mimeType.resize(mimeType.size() - 1);
+    }
+    co_return mimeType;
+}
+
+} // namespace
+
 Server::FilesystemResource::~FilesystemResource() = default;
 
 Awaitable<void> Server::FilesystemResource::getAsync(Response &response, Request &request)
@@ -29,13 +68,7 @@ Awaitable<void> Server::FilesystemResource::getAsync(Response &response, Request
     }
 
     /* Set the MIME type. */
-    {
-        std::string mimeType = co_await Subprocess::getStdout(ioc, "file", {"-bEL", "--mime-type", filePath.string()});
-        if (!mimeType.empty() && mimeType.back() == '\n') {
-            mimeType.resize(mimeType.size() - 1);
-        }
-        response.setMimeType(std::move(mimeType));
-    }
+    response.setMimeType(co_await getMimeTypeForFile(ioc, filePath));
 
     /* Write the file to the response. */
     Util::File file(ioc, std::move(filePath));
