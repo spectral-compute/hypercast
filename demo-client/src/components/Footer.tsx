45ba0f6c9733568746bfbfc01e62d4c912a695b4
Andrei Stepanenko <ftvkyo@gmail.com>
2023-04-03 17:39:16 +0100

    demo: RISE logo, footer updates, better Path styling

--- demo-client/src/components/Footer.tsx
+++ demo-client/src/components/Footer.tsx
@@ -1,26 +1,34 @@
 import "./Footer.scss";
 
+function Spec_a({location, children}: {location: string, children: React.ReactNode}) {
+    return <a
+        href={"https://spectralcompute.co.uk/" + location}
+        target="_blank"
+        rel="noreferrer"
+    >{children}</a>;
+}
+
 export default function Footer() {
     return <footer>
-        <div className="content-width">
-            <div className="footer-block">
+        <div className="content-width footer">
+            <div>
+                <span className="title">Product</span>
+                <ul>
+                    <li><Spec_a location="rise">Information</Spec_a></li>
+                </ul>
+            </div>
+            <div>
                 <span className="title">Company</span>
                 <ul>
-                    <li><a
-                        href="https://spectralcompute.co.uk/company"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        About
-                    </a></li>
-                    <li><a
-                        href="https://spectralcompute.co.uk/#contact"
-                        target="_blank"
-                        rel="noreferrer"
-                    >
-                        Contact
-                    </a></li>
+                    <li><Spec_a location="company">About</Spec_a></li>
+                    <li><Spec_a location="#contact">Contact</Spec_a></li>
                 </ul>
+            </div>
+            <div className="credits">
+                <span>by</span>
+                <Spec_a location="">
+                    <img src="/img/spectral-compute-logo.svg" alt="Spectral Compute" />
+                </Spec_a>
             </div>
         </div>
     </footer>;
--- demo-client/src/components/Header.tsx
+++ demo-client/src/components/Header.tsx
@@ -4,17 +4,8 @@ import "./Header.scss";
 export default function Header() {
     return <header>
         <div className="content-width">
-            <div className="title">Rapid Internet Streaming Engine (RISE)</div>
-            <div className="credits">
-                <span>by</span>
-                <a
-                    href="https://spectralcompute.co.uk/"
-                    target="_blank"
-                    rel="author noreferrer"
-                >
-                    <img src="/img/spectral-compute-logo.svg" alt="Spectral Compute" />
-                </a>
-            </div>
+            <img src="/img/rise-logo.svg" />
+            <div className="title">Rapid Internet Streaming Engine</div>
         </div>
     </header>;
 }
