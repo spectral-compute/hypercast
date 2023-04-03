41e2cfc315a415b8f5c56c53bde3aca6be6b56b2
Andrei Stepanenko <ftvkyo@gmail.com>
2023-04-03 14:50:07 +0100

    demo: add footer links

--- demo-client/src/components/Footer.tsx
+++ demo-client/src/components/Footer.tsx
@@ -3,9 +3,25 @@ import "./Footer.scss";
 export default function Footer() {
     return <footer>
         <div className="content-width">
-            <p>
-                Put something pretty here to look more serious.
-            </p>
+            <div className="footer-block">
+                <span className="title">Company</span>
+                <ul>
+                    <li><a
+                        href="https://spectralcompute.co.uk/company"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        About
+                    </a></li>
+                    <li><a
+                        href="https://spectralcompute.co.uk/#contact"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        Contact
+                    </a></li>
+                </ul>
+            </div>
         </div>
     </footer>;
 }
