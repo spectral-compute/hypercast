64f5c8d4a9d8c1768fb98986bff0506d057ba1e9
Chris Kitching <chriskitching@linux.com>
2023-04-05 16:31:02 +0100

    Cool motion blur on the loading animation

--- settings-ui/src/Kaput.tsx
+++ settings-ui/src/Kaput.tsx
@@ -6,11 +6,23 @@ export interface KaputProps {
     message?: string;
 }
 
+function spinner() {
+    // Well, this is insane.
+    let is = [];
+    for (let i = 0; i < 15; i++) {
+        is.push(i);
+    }
+
+    return <div className="happyContainer">{
+        is.map((i) => <Logo className={"happy " + " logo" + i} width="8em" height="8em"/>)
+    }</div>;
+}
+
 export default (props: KaputProps) => {
     const happy = props.message == "Loading...";
 
     return <div className="kaput">
-        {happy ? <Logo className="happy" width="8em" height="8em"/> : <Sadface width="5em" height="5em"/>}
+        {happy ? spinner() : <Sadface width="5em" height="5em"/>}
         {props.message ?? "A fatal error occurred :("}
     </div>;
 };
