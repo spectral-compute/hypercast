589a17e1ae84572495a2510cac1810238a1d4a47
Zac Tredger <zactredger@gmail.com>
2023-04-05 17:05:54 +0100

    demo: Fix linter complaint

--- demo-client/src/pages/PageTemplate.tsx
+++ demo-client/src/pages/PageTemplate.tsx
@@ -1,9 +1,11 @@
+import {ReactNode} from "react";
+
 import Header from "../components/Header";
 import Body from "../components/Body";
 import Footer from "../components/Footer";
 import Path from "../components/nav/Path";
 
-export default function ({children}: {children?: React.ReactNode}) {
+export default function ({children}: {children?: ReactNode}) {
     return <>
         <Header/>
         <Body>
