3208cf03b099f435c93ac1671b4233b473cafba1
Nicholas Tomlinson <nstomlinson@gmail.com>
2024-03-15 15:19:17 +0000

    [Server] Refactor main() to fix exception destruction order
    Otherwise, the state (which contains the log, amongst other things)
    can be destroyed before all the coroutines terminate. This leads to
    a segfault.

--- server/main.cpp
+++ server/main.cpp
@@ -27,63 +27,66 @@ Config::Root loadConfig(const std::filesystem::path &path)
     return Config::Root::fromJson(std::string_view((const char *)bytes.data(), bytes.size()));
 }
 
-/**
- * Asynchronous version of main.
- */
-Awaitable<void> asyncMain(int argc, const char * const *argv, IOContext &ioc)
-{
-    /* Read the configuration. */
-    if (argc != 2) {
-        throw std::runtime_error("Usage: "s + (argc ? argv[0] : "") + " configuration.json");
-    }
-    std::filesystem::path configPath = argv[1];
-
-    // Load and populate a config object.
-    Config::Root config = loadConfig(configPath);
-
-    Instance::State st{config, ioc};
-
-    /* Create global resources for the API. */
-    st.getServer().addResource<Api::ConfigResource>("api/config", st, configPath);
-#ifndef NDEBUG
-    st.getServer().addResource<Api::FullConfigResource>("api/full_config", st.getConfiguration());
-#endif // NDEBUG
-    st.getServer().addResource<Api::ProbeResource>("api/probe", ioc, st.getInUseUrls());
-
-    /* Create other instance global resources. */
-    if (config.features.channelIndex) {
-        st.getServer().addResource<Instance::ChannelsIndexResource>("channelIndex.json",
-                                                                    st.getConfiguration().channels);
-    }
-
-    /* Run the configuration application process. */
-    co_await st.applyConfiguration(config);
-
-    // Hang this coroutine forever. Interesting things happen as a result of the server handling requests
-    // in another coroutine.
-    Event event(ioc);
-    while (true) {
-        co_await event.wait();
-    }
-}
-
 } // namespace
 
 int main(int argc, const char * const *argv)
 {
-    /* This is just a wrapper around boost::asio and asyncMain. */
-    IOContext ioc;
-    spawnDetached(ioc, [&]() -> Awaitable<void> {
-        try {
-            co_await asyncMain(argc, argv, ioc);
+    try {
+        /* Parse the argument. */
+        if (argc != 2) {
+            throw std::runtime_error("Usage: "s + (argc ? argv[0] : "") + " configuration.json");
         }
-        catch (const std::exception &e) {
-            fprintf(stderr, "Exited with exception: %s\n", e.what());
+        std::filesystem::path configPath = argv[1];
+
+        /* Load and populate a config object. */
+        Config::Root config = loadConfig(configPath);
+
+        /* Create the instance state. */
+        IOContext ioc;
+        Instance::State st{config, ioc};
+
+        /* Create global resources for the API. */
+        st.getServer().addResource<Api::ConfigResource>("api/config", st, configPath);
+#ifndef NDEBUG
+        st.getServer().addResource<Api::FullConfigResource>("api/full_config", st.getConfiguration());
+#endif // NDEBUG
+        st.getServer().addResource<Api::ProbeResource>("api/probe", ioc, st.getInUseUrls());
+
+        /* Create other instance global resources. */
+        if (config.features.channelIndex) {
+            st.getServer().addResource<Instance::ChannelsIndexResource>("channelIndex.json",
+                                                                        st.getConfiguration().channels);
         }
-        catch (...) {
-            fprintf(stderr, "Exited with an unknown exception.\n");
-        }
-    });
-    ioc.run();
-    return 1; // The program should run indefinitely unless there's an error.
+
+        /* Do the asynchronous stuff. */
+        spawnDetached(ioc, [&]() -> Awaitable<void> {
+            try {
+                // Run the configuration application process.
+                co_await st.applyConfiguration(config);
+
+                // Hang this coroutine forever. Interesting things happen as a result of the server handling requests
+                // in another coroutine.
+                Event event(ioc);
+                while (true) {
+                    co_await event.wait();
+                }
+            }
+            catch (const std::exception &e) {
+                fprintf(stderr, "Exited with exception: %s\n", e.what());
+            }
+            catch (...) {
+                fprintf(stderr, "Exited with an unknown exception.\n");
+            }
+        });
+        ioc.run();
+    }
+    catch (const std::exception &e) {
+        fprintf(stderr, "Exited with exception: %s\n", e.what());
+    }
+    catch (...) {
+        fprintf(stderr, "Exited with an unknown exception.\n");
+    }
+
+    /* The program should run indefinitely unless there's an error. */
+    return 1;
 }
